9e1a6f24089a6ef4d316521c6df9cb47f080a7db
IRAYYA <IRAYYA@192.168.47.1>
2024-03-22 12:11:36 +0530

    add new project

--- ATIVAS/src/test/java/com/ativas/testscript/ProjectModule.java
+++ ATIVAS/src/test/java/com/ativas/testscript/ProjectModule.java
@@ -1,5 +1,14 @@
 package com.ativas.testscript;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class ProjectModule {
+	@Test
+	public void createProject()
+	{
+		Reporter.log("CreateCustomer",true);
+	}
+
 
 }
